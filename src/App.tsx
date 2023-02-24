0f6f79b14d8894b4a63788b471a96b1bd25763e0
dough-land <viktor.melin@me.com>
2023-02-24 13:19:49 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return <div className='App'></div>;
+};
+
+export default App;
